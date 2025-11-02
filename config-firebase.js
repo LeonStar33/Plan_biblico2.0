aee29a3d58bcaeb5e23f8bd2ee1d086d1b2eb5df
LeonEstrellaR <112821414+LeonStar33@users.noreply.github.com>
2025-11-02 01:50:47 -0600

    Update Firebase configuration settings

--- config-firebase.js
+++ config-firebase.js
@@ -1,5 +1,10 @@
+// Este es el ÚNICO contenido del archivo config-firebase.js
+
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  // ...etc...
+  apiKey: "AIzaSyCIk0GAQoBaLjNijBgCK-gRZRQDNkULy2g",
+  authDomain: "biblia-para-principiantes.firebaseapp.com",
+  projectId: "biblia-para-principiantes",
+  storageBucket: "biblia-para-principiantes.firebasestorage.app",
+  messagingSenderId: "262001109489",
+  appId: "1:262001109489:web:4b968123c5a291e9267c50"
 };
